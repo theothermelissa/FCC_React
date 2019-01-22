319951c0122b7268b8e09ef671ead9378eb95ef5
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-01-22 13:26:08 -0600

    Trying to keep track of conditional buttons is not my favorite.

---
+++ UseTernaryExpressionForConditionalRendering.js
@@ -0,0 +1,49 @@
+
+const inputStyle = {
+  width: 235,
+  margin: 5
+}
+
+class CheckUserAge extends React.Component {
+  constructor(props) {
+    super(props);
+    // change code below this line
+      this.state = {
+        input: "",
+        userAge: ""
+      }
+    // change code above this line
+    this.submit = this.submit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value,
+      userAge: "",
+    });
+  }
+  submit() {
+    this.setState({
+      userAge: this.state.input,
+      input: "",
+    }); 
+  }
+  render() {
+    const buttonOne = <button onClick={this.submit}>Submit</button>;
+    const buttonTwo = <button onClick={this.submit}>You May Enter</button>;
+    const buttonThree = <button onClick={this.submit}>You Shall Not Pass</button>;
+    return (
+      <div>
+        <h3>Enter Your Age to Continue</h3>
+        <input
+          style={inputStyle}
+          type="number"
+          value={this.state.input}
+          onChange={this.handleChange} /><br />
+        {
+          this.state.userAge === "" ? buttonOne : this.state.userAge >= 18 ? buttonTwo : buttonThree 
+        }
+      </div>
+    );
+  }
+};

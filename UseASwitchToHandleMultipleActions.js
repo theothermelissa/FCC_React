cce50db9c8523cabb9850a2563239c3775648eca
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-01-23 15:14:15 -0600

    Changes authentication in state using a switch statement

---
+++ UseASwitchToHandleMultipleActions.js
@@ -0,0 +1,31 @@
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+  // change code below this line
+  let thing = action.type;
+  switch(thing) {
+    case "LOGIN":
+      return {authenticated: true}
+    case "LOGOUT":
+      return {authenticated: false}
+    default: 
+      return defaultState;
+  }
+  // change code above this line
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: 'LOGOUT'
+  }
+};

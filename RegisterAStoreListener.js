873c1789fd521a714688d1754fa7d386b2ef329b
Melissa Morgan <melissapearlmorgan@gmail.com>
2019-01-24 09:18:51 -0600

    Listeners and Sending Data with Actions

---
+++ RegisterAStoreListener.js
@@ -0,0 +1,30 @@
+const ADD = 'ADD';
+
+const reducer = (state = 0, action) => {
+  switch(action.type) {
+    case ADD:
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
+const store = Redux.createStore(reducer);
+
+// global count variable:
+let count = 0;
+
+// change code below this line
+function handleChange() {
+  count = count + 1;
+}
+
+const makeChange = store.subscribe(handleChange);
+// change code above this line
+
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
+store.dispatch({type: ADD});
+console.log(count);
